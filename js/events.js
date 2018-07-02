7e5802d2647f080cb3c67115fac021b99c4e710b
Evangel Teo <evangelteo@gmail.com>
2018-07-02 08:21:22 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,15 @@
 //define functions here
 
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!");
+    return;
+  })
+}
+
+function frameIt() {
+  $(".tasty").on('load', function() {})
+}
 $(document).ready(function(){
 
 // call functions here
