6c061a5194ea5f04709a0c98fdd04a0fab642043
Evangel Teo <evangelteo@gmail.com>
2018-07-02 09:24:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,8 +12,8 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form').on('keydown', function(key) {
-    if(key.which == )
+  $('input').on('keydown', function(key) {
+    if(key.which == 71)
   })
 }
 $(document).ready(function(){
